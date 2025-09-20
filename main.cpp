019d46748c2f09ae0470338071cefe1b9a7f8bf0
metalcranium <metal_cranium07@yahoo.com>
2025-09-20 00:26:05 -0500

    wallpapers work

--- main.cpp
+++ main.cpp
@@ -15,12 +15,17 @@ class Button {
     Button(){
       width = 96;
       height = 96;
+      position = {0,0};
+      source = {position.x, position.y, width, height};
       
     }
     ~Button(){
     }
     void set_path(std::string wallpaper_path){
       path = wallpaper_path;
+    }
+    void print_position(){
+      std::cout << position.x << "," << position.y << std::endl;
     }
 };
 
@@ -57,6 +62,11 @@ void load_buttons(std::vector<Button>&buttons,std::vector<std::string>files){
     std::cout << count << " buttons loaded." << std::endl;
   }
 }
+void set_paths_to_buttons(std::vector<Button>&buttons,std::vector<std::string>files){
+  for (int i = 0; i < buttons.size(); i++){
+    buttons[i].set_path(files[i]);
+  }
+}
 
 void unload_textures(std::vector<Texture> pictures){
   for (auto i : pictures){
@@ -77,13 +87,37 @@ int main() {
 
   load_textures(wallpapers, files);
   load_buttons(buttons, files);
+  std::cout << "buttons: " << buttons.size() << std::endl;
+  
   int index = 0;
-  Vector2 dest = {0,0};
+  std::cout << "buttons loop" << std::endl;
+      for (int i = 0; i < buttons.size(); i++){
+        for (int j = 0; j < 5; j++){
+          buttons[index].position.x = j * 96;
+          buttons[index].position.y = i * 96;
+          buttons[index].source = {buttons[index].position.x, buttons[index].position.y,
+              buttons[index].width, buttons[index].height};
+          // buttons[index].print_position();
+          index++;
+         }
+      }
+  set_paths_to_buttons(buttons, files);
+
+  index = 0;
   while (!WindowShouldClose()){
     // update
+    Vector2 mouse = GetMousePosition();
     for (int i = 0; i < buttons.size(); i++){
-      if (CheckCollisionPointRec(GetMousePosition(), buttons[i].source)){
+      if (CheckCollisionPointRec(mouse, buttons[i].source)
+            && IsMouseButtonPressed(MOUSE_LEFT_BUTTON)){
         std::cout << "button: " << i << std::endl;
+        std::cout << "path: " << buttons[i].path << std::endl;
+        std::string preload = "hyprctl hyprpaper preload ";
+        std::string wallpaper = "hyprctl hyprpaper wallpaper ,";
+        std::string command = preload + buttons[i].path;
+        system(command.c_str());
+        command = wallpaper + buttons[i].path;
+        system(command.c_str());
       }
     }
 
@@ -92,6 +126,7 @@ int main() {
     ClearBackground(BLACK);
       for (int i = 0; i < wallpapers.size(); i++){
         for (int j = 0; j < 5; j++){
+          // buttons[index].position = {float(j*96), float(i*96)};
           Rectangle source = {0,0,float(wallpapers[index].width),float(wallpapers[index].height)};
           buttons[index].destination = {float(j*96), float(i*96), 96, 96};
           DrawTexturePro(wallpapers[index], source, buttons[index].destination
